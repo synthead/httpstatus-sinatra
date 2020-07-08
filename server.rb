e26dd4fa1d28bfa7b44e4ae77ec43f5b4ab3f01d
Maxwell Pray <synthead@gmail.com>
2020-07-08 16:56:12 -0700

    Use params var instead of passing parameters around.

--- server.rb
+++ server.rb
@@ -6,10 +6,13 @@ HEADERS = { "Content-Type" => "text/plain" }
 
 get "/:status" do
   sleep params["sleep"].to_i
-  halt params["status"].to_i, HEADERS, body_content(status)
+  halt params["status"].to_i, HEADERS, body_content
 end
 
-def body_content(status)
-  status_description = Rack::Utils::HTTP_STATUS_CODES[params["status"].to_i]
+def body_content
   [params["status"], status_description].compact.join(" ")
+end
+
+def status_description
+  Rack::Utils::HTTP_STATUS_CODES[params["status"].to_i]
 end
